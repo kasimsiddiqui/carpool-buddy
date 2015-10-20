7778057cc3238695268e7c824cb9fac48491ac63
Erik Luetkehans <eluetekhans@gmail.com>
2015-10-19 22:44:09 -0700

    bulk of trip routes done

--- routes/trip_routes.js
+++ routes/trip_routes.js
@@ -4,6 +4,7 @@ var User = require(__dirname + "/../models/user");
 var jsonParser = require('body-parser').json();
 var handleError = require(__dirname + '/../lib/handle_error');
 var dateParser = require(__dirname + '/../lib/date_parser');
+var findDistance = require(__dirname + '/../app/js/find_distance');
 
 var tripsRoute = module.exports = exports = express.Router();
 
@@ -19,24 +20,58 @@ tripsRoute.get('/trips', jsonParser, function(req, res) {
     var leastTimeDest = dateParser(search.destTime) - (1000 * 60 * 30);
     var mostTimeDest = dateParser(search.destTime) + (1000 * 60 * 30);
     // TODO add day of week support and origin/dest support
-    Trip.find({$and: [{"origin": search.origin}, {"dest": search.dest},
-                      {$and: [{"originTime": {$gt: leastTimeOrigin}},
+    Trip.find({$and: [{$and: [{"originTime": {$gt: leastTimeOrigin}},
                               {"originTime": {$lt: mostTimeOrigin}}
                       ]},
                       {$and: [{"destTime": {$gt: leastTimeDest}},
                               {"destTime": {$lt: mostTimeDest}}
                       ]}
               ]}, function(err, docs) {
-      if (err) handleError(err, res);
-      res.json({trips: docs});
+      if (err) handleError(err, res, 500);
+      destMatch = [];
+      docs.forEach(function(doc) {
+        console.log(findDistance(search.origin, doc.origin));
+        if(findDistance(search.origin, doc.origin) < 5 &&
+           findDistance(search.dest, doc.dest) < 5) {
+          destMatch.push(doc);
+        }
+      });
+
+      res.json({trips: destMatch});
     });
   }
   // Otherwise we are finding all the trips the user is a part of.
   User.findOne({email: req.body.userEmail}, function(err, user) {
-    if (err) handleError(err);
+    if (err) handleError(err, res, 500);
     Trip.find({travelers: user._id}, function(err, docs) {
-      if (err) handleError(err);
+      if (err) handleError(err, res, 500);
       res.json({trips: docs});
     });
+  });
+});
+
+tripsRoute.post('/trips', jsonParser, function(req, res) {
+  var tripInfo = req.body.trip;
+  User.findOne({email: req.body.trip.userEmail}, function(err, user) {
+    var trip = new Trip();
+    trip.tripName = tripInfo.tripName;
+    trip.origin = tripInfo.origin;
+    trip.originTime = dateParser(tripInfo.originTime);
+    trip.dest = tripInfo.dest;
+    trip.destTime = dateParser(tripInfo.destTime);
+    trip.weekDays = tripInfo.weekDays;
+    trip.travelers = [user._id];
+    trip.seatsLeft = user.carSeats -1;
+    trip.save(function(err, data) {
+      debugger;
+      if (err) handleError(err, res, 500);
+    });
+  });
+});
+
+tripsRoute.delete('/trips', jsonParser, function(req, res) {
+  Trip.remove({_id: req.body.tripId}, function(err) {
+    if (err) return handleError(err, res, 500);
+    res.json({msg: 'success'});
   });
 });
--- test/api/trip_route_test.js
+++ test/api/trip_route_test.js
@@ -4,18 +4,18 @@ chai.use(chaiHttp);
 var expect = chai.expect;
 process.env.MONGODB_URL = 'mongodb://localhost/carpool_test';
 var mongoose = require('mongoose');
-var Trip = require(__dirname + '/../models/trip');
-var User = require(__dirname + '/../models/user');
+var Trip = require(__dirname + '/../../models/trip');
+var User = require(__dirname + '/../../models/user');
 var url = 'localhost:3000/api';
 
-require(__dirname + '/../server');
+require(__dirname + '/../../server');
 
 describe('Trip routes', function() {
   before(function(done) {
     //populate a trip to search for
     var trip = new Trip();
-    trip.origin = "origin";
-    trip.dest = "dest";
+    trip.origin = "Bellevue, WA";
+    trip.dest = "Seattle, WA";
     trip.originTime = 8 * 1000 * 60 * 60;
     trip.destTime = 20 * 1000 * 60 * 60;
     trip.travelers = [mongoose.Types.ObjectId('560d88ab95136958181e421f')];
@@ -24,6 +24,7 @@ describe('Trip routes', function() {
       var user = new User();
       user.email = "test email";
       user._id = mongoose.Types.ObjectId('560d88ab95136958181e421f');
+      user.carSeats = 4;
       user.save(function(err, data) {
         if (err) throw err;
         done();
@@ -39,7 +40,7 @@ describe('Trip routes', function() {
   });
 
   it('should search +/- 30 min', function(done) {
-    var tripSearch = {"origin": "origin", "dest": "dest",
+    var tripSearch = {"origin": "Bellevue, WA", "dest": "Seattle, WA",
                       "originTime": "07:59 AM", // 29 min under
                       "destTime": "08:01 PM"}; // 29 min over
     chai.request(url)
@@ -47,7 +48,7 @@ describe('Trip routes', function() {
       .send({tripSearch: tripSearch})
       .end(function(err, res) {
         expect(err).to.eql(null);
-        expect(res.body.trips[0].origin).to.eql("origin");
+        expect(res.body.trips[0].origin).to.not.eql(undefined);
         done();
       });
   });
@@ -58,8 +59,27 @@ describe('Trip routes', function() {
       .send({userEmail: "test email"})
       .end(function(err, res) {
         expect(err).to.eql(null);
-        expect(res.body.trips[0].origin).to.eql("origin");
+        expect(res.body.trips[0].origin).to.not.eql(undefined);
         done();
+      });
+  });
+
+  it('should be able to make a new trip', function(done) {
+    chai.request(url)
+      .post('/trips')
+      .send({"trip": {"tripName": "test name", "origin": "Renton, WA", 
+                    "originTime": "09:00 AM", "dest": "Everett, WA",
+                    "destTime": "10:00 AM", "weekDays": "mon",
+                    "userEmail": "test email"}}
+            )
+      .end(function(err, res) {
+        debugger;
+        Trip.findOne({origin: "Renton, WA"}, function(err, doc) {
+          expect(err).to.eql(null);
+          debugger;
+          expect(doc.dest).to.eql("Everett, WA");
+          done();
+        });        
       });
   });
 });
